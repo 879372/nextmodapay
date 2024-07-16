83020375f92ddbbca3b2f959e5dbcb24531e6557
José Kaio <126286274+879372@users.noreply.github.com>
2024-07-16 09:10:00 -0300

    Add files via upload

--- api/listarPixOut.ts
+++ api/listarPixOut.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { API_BASE_URL } from "./cadastro";
 import axios from 'axios';
 
@@ -62,62 +61,4 @@ export const listPixOutByCompany = async (params: PixOutSearchParams, token: str
     throw error; // Lance o erro para que ele possa ser tratado na chamada da função
   }
 }
-=======
-import { API_BASE_URL } from "./cadastro";
-import axios from 'axios';
 
-export interface TransacaoOut {
-    idTrasacao: string;
-    chave: string;
-    descricao: string;
-    solicitacao: string;
-    pagador: {
-        cpf: string;
-        nome: string;
-    };
-    valor: {
-        original: number;
-    };
-    endToEndId: string;
-    comprovante: string;
-    status: string;
-    recebedor: {
-        documento: string;
-        nome: string;
-        data: string;
-    };
-}
-
-export interface PixOutSearchParams{
-    inicio?: string;
-    fim?: string;
-    cpf?: string;
-    status?: string;
-    paginaAtual?: number;
-    itensPorPagina?: number;
-}
-
-export const listPixOutByCompany = async (params: PixOutSearchParams, token: string): Promise<TransacaoOut[]> =>{
-    const { inicio, fim, cpf, status, paginaAtual, itensPorPagina} = params
-    try{
-        const response = await axios.get(`${API_BASE_URL}api/adm/pix-out/`,{
-            headers:{
-                Authorization: `Bearer ${token}`,
-            },
-            params:{
-                inicio,
-                fim,
-                cpf,
-                status,
-                paginaAtual,
-                itensPorPagina
-            }
-        })
-        return response.data
-    } catch (error){
-        console.error("Pix In:", error)
-        return [];
-    }
-}
-
->>>>>>> c19ed366fb65f48d4532a2dba833eda2f63bcb5e
